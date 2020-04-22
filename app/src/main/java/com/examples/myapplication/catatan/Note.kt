4c0cba824c2f6edf189659de00bd74b7b35ad19d
githubuser-44 <hanif_ginaris@yahoo.com>
2020-04-22 23:52:51 +0700

    10

---
+++ app/src/main/java/com/examples/myapplication/catatan/Note.kt
@@ -0,0 +1,15 @@
+package com.examples.myapplication.catatan
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(
+    var title: String,
+    var description: String,
+    var nomer: String,
+    var priority: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
